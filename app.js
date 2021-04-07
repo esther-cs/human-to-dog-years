2f182525e596f5a3a00aacbff717609674e7f06d
Esther O <64683541+esther-cs@users.noreply.github.com>
2021-04-07 15:47:12 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+var myAge=13
+//my age
+let earlyYears=2
+//Early Years
+earlyYears*=10.5
+let laterYears=myAge-2
+laterYears*=4
+console.log(earlyYears)
+console.log(laterYears)
+var myAgeInDogYears=earlyYears+laterYears
+var myName = 'Esther Or'.toLowerCase()
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
